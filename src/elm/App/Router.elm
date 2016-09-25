e658577eebb6524e5f352a7de2a587df1060d5f0
Mark Huo <huomian@outlook.com>
2016-09-25 16:31:23 +0800

    remove Builder

--- src/elm/App/Router.elm
+++ src/elm/App/Router.elm
@@ -2,7 +2,6 @@ module App.Router exposing (delta2url, hash2messages)
 
 import Navigation exposing (Location)
 import RouteUrl exposing (HistoryEntry(..), UrlChange)
-import RouteUrl.Builder as Builder exposing (Builder)
 import App.Model exposing (Page(..), Model)
 import App.Update exposing (Msg(..))
 
